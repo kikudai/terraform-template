630d5783b4134a0e37b13fd1c63bb82263b4e024
Daichi Kikuchi <king.show.kikudai@gmail.com>
2025-02-27 02:01:14 +0900

    EC2 Windows の RDP 許可追加

--- aws-ec2-windows-ad/userdata.ps1
+++ aws-ec2-windows-ad/userdata.ps1
@@ -13,4 +13,13 @@ $AdminCred = New-Object System.Management.Automation.PSCredential ("Administrato
 # ドメインコントローラー昇格
 Install-ADDSForest -DomainName $domainName -DomainNetbiosName $NetBIOSName -SafeModeAdministratorPassword $AdminPassword -InstallDns -Confirm:$false -Force
 
+# 🔹 RDP を有効化
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
+
+# 🔹 Windows ファイアウォールで RDP を許可
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+# 🔹 ネットワークレベル認証 (NLA) を無効化 (必要に応じて)
+# Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name "UserAuthentication" -Value 0
+
 Restart-Computer
