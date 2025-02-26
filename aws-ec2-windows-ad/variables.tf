d8ce8ebc71659f24cd71ec3b8606072fa67cdf7d
Daichi Kikuchi <king.show.kikudai@gmail.com>
2025-02-27 01:08:34 +0900

    不足変数を追加

--- aws-ec2-windows-ad/variables.tf
+++ aws-ec2-windows-ad/variables.tf
@@ -10,3 +10,13 @@ variable "key_name" {
   description = "EC2 にアクセスするための SSH キーペア"
   default     = "windows_ad_key"
 }
+
+variable "availability_zone" {
+  description = "EC2 インスタンスを起動するアベイラビリティゾーン"
+  default     = "ap-northeast-1a"  # デフォルトの AZ
+}
+
+variable "spot_price" {
+  description = "スポットインスタンスの最大価格"
+  default     = "0.0357"  # デフォルトのスポット価格
+}
