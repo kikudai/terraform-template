4a878a3969762c46217087c1c6e321239c179151
Daichi Kikuchi <king.show.kikudai@gmail.com>
2025-03-16 20:50:39 +0900

    出力追加

--- environments/poc/outputs.tf
+++ environments/poc/outputs.tf
@@ -6,4 +6,12 @@ output "windows_ad_private_ip" {
   value = module.compute.windows_ad_private_ip
 }
 
-# 他の必要な出力値を追加 
+output "vpn_endpoint_dns" {
+  description = "VPNエンドポイントのDNS名"
+  value = module.vpn.vpn_endpoint_dns
+}
+
+output "get_windows_password_command" {
+  value = "aws ec2 get-password-data --instance-id ${module.compute.windows_ad_instance_id} --priv-launch-key ../../modules/compute/windows_ad_key.pem"
+  description = "Command to retrieve the Windows administrator password"
+}
